e98e728af92c15c21a7e1b01546e2af13ca2142d
Pol Enault <proust96@yahoo.fr>
2018-11-24 07:21:23 +0100

    ajustement icones pour layout

--- script2.js
+++ script2.js
@@ -4,6 +4,10 @@ $( document ).ready(function() {
     });
 
     $( ".hexagon_container" ).draggable({ revert: "invalid",stop: function() {
+        let icon = $(this).children(".hexagon").children(".svg_icon").removeClass("svg_icon").addClass("svg_iconAbso");
+        let text = $(this).children(".hexagon").children(".text_icon").removeClass("text_icon").addClass("text_iconAbso");;
+        icon.appendTo($(this));
+        text.appendTo($(this));
         $(this).removeClass("hexagon_container").css("top","0").css("left", "0").children(".hexagon").replaceWith('<img class="imageAbso" src="img/hexa.png" />');
       } });
     $( ".droppable" ).droppable({
