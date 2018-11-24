0a0f74597952e335d6be08bb9ac32647e9c8e636
Pol Enault <proust96@yahoo.fr>
2018-11-24 08:06:21 +0100

    index joli

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 $( document ).ready(function() {
     $(".hexagon").click(function(){
+        if ($(this).hasClass("selected")){
+            $(this).children('.svg_icon').children("img").attr("src",$(this).children('.svg_icon').children("img").attr("src").replace("blanc.png", "bleu.png"));
+        }
+        else{
+            $(this).children('.svg_icon').children("img").attr("src",$(this).children('.svg_icon').children("img").attr("src").replace("bleu.png", "blanc.png"));
+        }
         $(this).toggleClass("selected");
+    });
+
+    $(".hexagon").hover(function(){
+        $(this).children('.svg_icon').children("img").attr("src",$(this).children('.svg_icon').children("img").attr("src").replace("bleu.png", "blanc.png"));
+    }, function(){
+        if (!$(this).hasClass("selected")){
+        $(this).children('.svg_icon').children("img").attr("src",$(this).children('.svg_icon').children("img").attr("src").replace("blanc.png", "bleu.png"));
+        }
     });
 });
