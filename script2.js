598ce71d61040c9f8fd6967fb0151f32dad4f7f2
Pol Enault <proust96@yahoo.fr>
2018-11-24 04:59:43 +0100

    dragdrop

--- script2.js
+++ script2.js
@@ -3,10 +3,14 @@ $( document ).ready(function() {
         $(this).toggleClass("selected");
     });
 
-    $( ".hexagon_container" ).draggable({ revert: "invalid" });
+    $( ".hexagon_container" ).draggable({ revert: "invalid",stop: function() {
+        $(this).removeClass("hexagon_container").css("top","0").css("left", "0").children(".hexagon").replaceWith('<img class="imageAbso" src="img/hexa.png" />');
+      } });
     $( ".droppable" ).droppable({
         classes: {
           "ui-droppable-active": "ui-state-active",
           "ui-droppable-hover": "ui-state-hover"
+        },drop: function(e, ui){
+            $(ui.draggable).appendTo($(this));
         }});
 });
